d7a188cecf6379ceabf245483cfc25154add9e2f
prince456321 <141109052+prince456321@users.noreply.github.com>
2026-04-28 21:31:31 +0200

    resolve ssh connection time-out

--- infra/terraform-music/main.tf
+++ infra/terraform-music/main.tf
@@ -166,7 +166,7 @@ resource "aws_security_group" "sg_serveur_musique_app" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_ssh_cidr]
+    cidr_blocks =  ["0.0.0.0/0"]
   }
 
   ingress {
